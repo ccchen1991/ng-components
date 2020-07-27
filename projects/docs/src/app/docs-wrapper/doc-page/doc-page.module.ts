1cfb63ee76938982e42b15736eb2783abbed2c03
Boudfoust <S8381B@corp1.ad1.seb.net>
2020-07-27 16:05:38 +0800

    feat: add try out on mobile device

--- projects/docs/src/app/docs-wrapper/doc-page/doc-page.module.ts
+++ projects/docs/src/app/docs-wrapper/doc-page/doc-page.module.ts
@@ -9,10 +9,11 @@ import { PlaygroundComponent } from "./playground/playground.component";
 import { DocNotesComponent } from "./notes/notes.component";
 import { CodeSnippetModule } from "../../common/code-snippet/code-snippet.module";
 import { DynamicFormModule } from "../../common/dynamic-form/dynamic-form.module";
+import { TooltipModule } from "../../../../../ng-components/lib/tooltip/tooltip.module";
 
 @NgModule({
     declarations: [DocPageComponent, PlaygroundComponent, APIsComponent, DocNotesComponent],
-    imports: [CommonModule, FormsModule, TabsModule, CodeSnippetModule, DynamicFormModule],
+    imports: [CommonModule, FormsModule, TabsModule, CodeSnippetModule, DynamicFormModule, TooltipModule],
     exports: [DocPageComponent, DynamicFormModule],
     providers: [APIExtractService],
 })
